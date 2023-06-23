ff28fb7fd59f2c3f16422e10f51f879aad8c622f
Ahmed Desoky <133752872+ahmeddesoky96@users.noreply.github.com>
2023-06-23 22:39:58 +0300

    Update settings.py

--- VEM_Django2/settings.py
+++ VEM_Django2/settings.py
@@ -101,9 +101,9 @@ DATABASES = {
 }
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kinddark1996@gmail.com'
-EMAIL_HOST_PASSWORD = 'xqyowciecfusfbuq'
+EMAIL_PORT = ****
+EMAIL_HOST_USER = '*************'
+EMAIL_HOST_PASSWORD = '********'
 EMAIL_USE_TLS = True
 
 ##### email: kinddark1996@gmail.com
