aebd326cdb610802171e86df7d94903b4ae6b624
Ahmed Desoky <ahmeddesoky412@gmail.com>
2023-07-03 15:13:05 +0300

    after test all functions

--- mall/urls.py
+++ mall/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path,re_path
+from django.views.generic import TemplateView
 from mall.views import *
 from django.conf import settings
 from django.conf.urls.static import static
